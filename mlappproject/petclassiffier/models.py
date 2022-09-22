7ff0628727e02a2acc174c5323cfbbfef998c2b8
miguel jaramillo <migueljaramillo879@gmail.com>
2022-09-21 21:20:27 -0500

    Creacion nuevo modelo y creacion de nuevo html

--- mlappproject/petclassiffier/models.py
+++ mlappproject/petclassiffier/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from platform import architecture
 
 # Create your models here.
+class mlModels(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    architecture = models.FileField(upload_to='mlmodels/') #json
+    weights = models.FileField(upload_to='mlmodels/') #h5 file
+    priority = models.PositiveSmallIntegerField(null=True)
